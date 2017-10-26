1fbc7125abbbbffdfe4fd3bd454587ea41270055
FERDEDDO <FERDEDDO@GMAIL.com>
2017-10-26 12:59:34 +0200

    first commit

---
+++ app/models/usel.rb
@@ -0,0 +1,2 @@
+class Usel < ApplicationRecord
+end
---
+++ db/migrate/20171026105813_create_usels.rb
@@ -0,0 +1,10 @@
+class CreateUsels < ActiveRecord::Migration[5.1]
+  def change
+    create_table :usels do |t|
+      t.string :email
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
